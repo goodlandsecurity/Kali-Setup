d87ffe08b9af9049c2ba3862bff1f67f834ed9e3
goodlandsecurity <josing.jj@gmail.com>
2020-11-08 12:32:26 -0600

    adding powerlevel10k theme and nerd font

--- modules/templates/zsh.py
+++ modules/templates/zsh.py
@@ -14,6 +14,14 @@ class InstallerTemplate:
 
         print_status("Installing oh-my-zsh", 1)
         run_command('curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | zsh')
+        print_status("Installing PowerLevel10k theme for oh-my-zsh", 1)
+        github_clone_option(option='--depth=1',
+                            repo='romkatv/powerlevel10k',
+                            dest_folder='{0}/.oh-my-zsh/custom/themes/powerlevel10k'.format(get_home_folder()))
+        print_status("Installing zsh-syntax-highlighting", 1)
+        github_clone_option(option='',
+                            repo='zsh-users/zsh-syntax-highlighting',
+                            dest_folder='{0}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting'.format(get_home_folder()))
         file_append_once('{0}/.zshrc'.format(get_home_folder()), 'setopt interactivecomments', 'interactivecomments')
         file_append_once('{0}/.zshrc'.format(get_home_folder()), 'setopt ignoreeof', 'ignoreeof')
         file_append_once('{0}/.zshrc'.format(get_home_folder()), 'setopt correctall', 'correctall')
@@ -21,8 +29,19 @@ class InstallerTemplate:
         file_append_once('{0}/.zshrc'.format(get_home_folder()), 'source $HOME/.aliases', '.aliases')
         if config.getboolean('general', '4k', fallback=False):
             file_append_once('{0}/.zshrc'.format(get_home_folder()), 'export GDK_SCALE=2')
-        file_replace('{0}/.zshrc'.format(get_home_folder()), 'ZSH_THEME=.*', 'ZSH_THEME="mh"')
-        file_replace('{0}/.zshrc'.format(get_home_folder()), 'plugins=.*', 'plugins=(git-extras tmux dirhistory python pip)')
+        file_replace('{0}/.zshrc'.format(get_home_folder()), 'ZSH_THEME=.*', 'ZSH_THEME="powerlevel10k/powerlevel10k"')
+        file_replace('{0}/.zshrc'.format(get_home_folder()),
+                     'plugins=.*', 'plugins=(git git-extras tmux zsh-syntax-highlighting dirhistory python pip)')
         file_append_once('{0}/.zshrc'.format(get_home_folder()), 'source $HOME/.xinitrc')
-        
+        print_success("Done!", 1)
+        print_status("Installing Meslo Nerd Font for Powerlevel10k!", 1)
+        make_dir('{0}/.fonts'.format(get_home_folder()))
+
+        font_list = ['MesloLGS NF Regular', 'MesloLGS NF Bold', 'MesloLGS NF Italic', 'MesloLGS NF Bold Italic']
+
+        for i in font_list:
+            url = 'https://github.com/romkatv/powerlevel10k-media/raw/master/{0}.ttf'.format(i)
+            file_download(url, '{0}/.fonts/{1}.ttf'.format(get_home_folder(), i))
+            
+        run_command('fc-cache -fv')
         print_success("Done!", 1)
